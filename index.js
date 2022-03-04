c12f0ef1d86c1840d86b4767755d1e2255c69b6c
St3ph4ne <spodar.s@protonmail.com>
2022-03-04 17:03:36 +0000

    server pour angular_dec

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var express = require('express')
 var app = express()
 
-app.get('/api', function (req, res) {
-  res.send('hello world')
+const messages = [
+  {
+    title : 'Titre du message 1',
+    content : 'contenu du message 1',
+    isRead: false,
+    sent: new Date()
+  },
+  {
+    title : 'Titre du message 2',
+    content : 'contenu du message 2',
+    isRead: false,
+    sent: new Date()
+  },
+  {
+    title : 'Titre du message 3',
+    content : 'contenu du message 3',
+    isRead: false,
+    sent: new Date()
+  },
+];
+
+app.get("/api", function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', "*")
+  res.setHeader('Content-Type', 'Application/json')
+  res.json(messages)
 })
 
-app.listen(4444)
+app.listen(8828)

5cfdb44ac4d05b79f60750167e36fe3de30e9cfd
St3ph4n3 <spodar.s@protonmail.com>
2022-03-07 16:59:58 +0000

    wip server with delete message

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var express = require('express')
+var express = require('express');
+const res = require('express/lib/response');
+var jwt = require('jsonwebtoken');
 var app = express()
 
 const messages = [
   {
+    id : 1,
     title : 'Titre du message 1',
     content : 'contenu du message 1',
     isRead: false,
     sent: new Date()
   },
   {
+    id : 2,
     title : 'Titre du message 2',
     content : 'contenu du message 2',
     isRead: false,
     sent: new Date()
   },
   {
+    id : 3,
     title : 'Titre du message 3',
     content : 'contenu du message 3',
     isRead: false,
@@ -22,10 +27,18 @@ const messages = [
   },
 ];
 
+
 app.get("/api", function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', "*")
   res.setHeader('Content-Type', 'Application/json')
   res.json(messages)
 })
 
+app.get("/user/token", function (req, res) {
+  var token = jwt.sign(USER, 'keyone', { expiresIn: "7 days" });
+  console.log(token);
+  res.send(token);
+})
+
 app.listen(8828)
+

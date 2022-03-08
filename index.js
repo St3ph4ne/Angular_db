310952d865865882b9d406f5202b81c275a372d5
St3ph4n3 <spodar.s@protonmail.com>
2022-03-08 17:37:03 +0000

    wip suite

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 const res = require('express/lib/response');
 var jwt = require('jsonwebtoken');
-var app = express()
+const logins = require('./logins.json')
+var app = express();
+var cors = require('cors');
+const SECRET = "secret";
+
+app.use(cors());
+app.use(express.json());
 
 const messages = [
   {
@@ -27,17 +33,33 @@ const messages = [
   },
 ];
 
-
+//envoie une requête et renvoie les messages sous forme de json
 app.get("/api", function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', "*")
   res.setHeader('Content-Type', 'Application/json')
   res.json(messages)
 })
 
-app.get("/user/token", function (req, res) {
-  var token = jwt.sign(USER, 'keyone', { expiresIn: "7 days" });
-  console.log(token);
-  res.send(token);
+//
+app.post("/api", function (req, res) {
+
+})
+
+
+//pour chaque user/mdp on vérifie que les données récupérées correspondent à ma liste local
+app.post("/logins", (req, res) => {
+  logins.forEach(element => { 
+      if (req.body.name == element.name && req.body.password == element.password) {
+          res.status(200);
+
+          //génération du token:
+          const token = jwt.sign({
+            username: element.name
+          }, SECRET, { expiresIn: '7 days' })
+          res.json({ access_token: token }) //renvoie en réponse le token au format json
+      }
+  })
+  res.status(401).send("identifiant invalide")
 })
 
 app.listen(8828)
